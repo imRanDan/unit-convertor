30595b9f3b0f7272febaa2bf0b2d2c73e2004ad6
Danyal Imran <dan.imran97@gmail.com>
2023-08-22 18:52:06 -0400

    added in functions to convert units to other units

--- index.js
+++ index.js
@@ -10,24 +10,35 @@ let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
 
 
-// function volumeConversion(n) {
-//     return (valueInput * 0.264)
-// }
 
-// function massConversion(n) {
-//     return (valueInput * 2.204)
-// }
+function metersToFeet(numInput) {
+    return (numInput * 3.281).toFixed(3)
+}
 
+function feetToMeters(numInput) {
+    return (numInput / 3.281).toFixed(3)
+}
 
+function litersToGallons(numInput) {
+    return (numInput * 0.264).toFixed(3)
+}
 
-function lengthConversion(numInput) {
-    return (numInput * 3.281)
+function gallonsToLiters(numInput) {
+    return (numInput / 0.264).toFixed(3)
+}
+
+function kilogramsToPounds(numInput) {
+    return (numInput * 2.204).toFixed(3)
+}
+
+function poundsToKilograms(numInput) {
+    return (numInput / 2.204).toFixed(3)
 }
 
 convertBtn.addEventListener("click", function() {
     numInput = valueEl.value
     valueEl.value = ""
-    lengthEl.innerText = `${numInput} meters = ${lengthConversion(numInput)} feet`
-        
-   
+    lengthEl.innerText = `${numInput} meters = ${metersToFeet(numInput)} feet | ${numInput} feet = ${feetToMeters(numInput)} meters`
+    volumeEl.innerText = `${numInput} liters = ${litersToGallons(numInput)} gallons | ${numInput} gallons = ${gallonsToLiters(numInput)} liters` 
+    massEl.innerText = `${numInput} kilograms = ${kilogramsToPounds(numInput)} pounds | ${numInput} pounds = ${poundsToKilograms(numInput)} kilograms`
 }) 
